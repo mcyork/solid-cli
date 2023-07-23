eb4134e199a713d85425ebb4cfd0f28de3db7a53
Ian McCutcheon <ian@mcyork.com>
2023-07-23 15:20:45 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+
+# Set up API credentials
+api_endpoint = "https://your-solidserver-endpoint"  # Replace with your actual API endpoint
+api_username = "your_username"  # Replace with your actual username
+api_password = "your_password"  # Replace with your actual password
+ca_cert_file = "my_ca_cert.pem"  # Replace with the path to your CA certificate file
+
+# Define the records to retrieve
+ip_addr = "192.168.0.100"
+a_record = "myserver1.example.com"
+cname_record = "myserver2.example.com"
